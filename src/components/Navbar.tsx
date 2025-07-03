17fad88dbfbd8f239145b075b131afb2251338b0
Shaan <shaanduss@gmail.com>
2025-07-03 22:57:19 +0800

    shadcn theming

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { motion } from "motion/react"
+
+export const Navbar: React.FC = () => {
+  return(
+    <div className="sticky top-0 w-full">
+      <nav>this is my navbar</nav>
+    </div>
+
+  )
+}
