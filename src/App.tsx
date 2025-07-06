e4cf20fe0b70fc0201fc23aa7318d4f9d06f8769
Shaan <shaanduss@gmail.com>
2025-07-07 02:13:57 +0800

    Feature: Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import WelcomeDialog from './components/WelcomeDialog'
+import { HomeView } from './views/HomeView'
 
 function App() {
   return (
-    <div>
-      <WelcomeDialog />
-      <Navbar/>
-      <div>
+    <Router>
+      <div className="min-h-screen flex-col">
+        <WelcomeDialog />
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   )
 }
 
