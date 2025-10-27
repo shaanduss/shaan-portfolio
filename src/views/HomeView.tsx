7929c0216a920916648ce95dad1b9c2aaa624f3d
Shaan <shaanduss@gmail.com>
2025-10-27 14:06:02 +0800

    Alert to indicate incompleteness of website

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,14 +1,20 @@
 import IntroCard from "@/components/home/introCard";
 import RecentExperiences from "@/components/home/recentExperiences";
 import SkillsCarousel from "@/components/home/skillsCarousel";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Loader } from "lucide-react";
 import React from "react";
 
 export const HomeView: React.FC = () => {
-  return(
+  return (
     <div className="flex flex-col justify-center items-center px-20 mt-7 gap-10">
+      <Alert variant="destructive">
+        <Loader />
+        <AlertTitle>Website still under construction...</AlertTitle>
+      </Alert>
       <IntroCard />
       <SkillsCarousel />
       <RecentExperiences />
     </div>
-  )
-}
+  );
+};
